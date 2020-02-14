9d451d502e0a6330f41ca95f50df9160b2eef07f
Gurzhii Dmitry <gurzhii.dmitriy@gmail.com>
2020-02-14 09:27:19 +0200

    refactoring controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,26 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+
 class PostsController extends Controller
 {
 
     public function show($id)
     {
-        $post = \DB::table('posts')
-            ->select([
-                'posts.id',
-                'posts.title',
-                \DB::raw('users.name as username')
-            ])
-            ->join('users', 'posts.user_id', '=', 'users.id')
-            ->where('posts.id', $id)->get();
-
-        $comments = \DB::table('comments')
-            ->select('post_id', 'text')
-            ->where('comments.post_id', $id)->get();
-
-        $post->comments = $comments;
-
-        return $post;
+        return Post::with(['owner', 'comments'])->findOrFail($id);
     }
 }
